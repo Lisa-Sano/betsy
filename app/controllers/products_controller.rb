1a2cf60b98e2f12699dc7c45d4e41bf9a79e74fd
Lisa Rolczynski <lisa.sano@gmail.com>
2016-05-03 20:26:38 -0700

    index shows a list of all products initially, and has a drop down to view by category. price and product name displayed.  product names are links to show pages for individual products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,16 @@
 class ProductsController < ApplicationController
+  def index
+    @categories = Product.uniq.pluck(:category).sort
+
+    if params[:category]
+      @products = Product.where(category: params[:category])
+    else
+      @products = Product.order(name: :asc)
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    @order_item = OrderItem.new(product_id: @product.id)
+  end
 end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,5 @@
 module ProductsHelper
+  def to_money(price_in_cents)
+    "$" + sprintf('%.2f', (price_in_cents / 100))
+  end
 end
