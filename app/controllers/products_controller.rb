20387054150459b486e9c7459a1641b801504ac1
Lisa Rolczynski <lisa.sano@gmail.com>
2016-05-03 21:25:51 -0700

    added merchant dropdown menu for browsing. added link back to all products if looking at a subset of products. updated controller to include scope to allow for searching by both category and merchant simultaneously

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,19 @@
 class ProductsController < ApplicationController
   def index
     @categories = Product.uniq.pluck(:category).sort
+    @merchants = User.joins(:products).uniq.sort
+
+    scope = Product.order(name: :asc)
 
     if params[:category]
-      @products = Product.where(category: params[:category])
-    else
-      @products = Product.order(name: :asc)
+      scope = scope.where(category: params[:category])
     end
+
+    if params[:merchant]
+      scope = scope.where(user_id: params[:merchant])
+    end
+
+    @products = scope
   end
 
   def show
