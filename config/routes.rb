775366f6ba38ab67afff855aec565286c7843c3c
Lisa Rolczynski <lisa.sano@gmail.com>
2016-05-03 21:26:35 -0700

    added routes for products#index and products#show

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  resources :products, :only => [:index, :show]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -12,7 +14,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+    # resources :products
 
   # Example resource route with options:
   #   resources :products do
