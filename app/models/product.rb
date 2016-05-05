ef58b6ed9f59bf417d84b447e2a9dd24a328a269
Lisa Rolczynski <lisa.sano@gmail.com>
2016-05-05 13:26:15 -0700

    added validations to Product for name (present, unique), price (present, integer, greater than 0), user_id (present), and stock (present)

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ActiveRecord::Base
   belongs_to :user
   has_many :reviews
   has_many :order_items
+
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :user_id, :stock, presence: true
 end
