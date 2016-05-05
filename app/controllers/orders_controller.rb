dd90df61a2b75a64ed2e361aab1df3acf8d49de7
Jillian Boshart <Jilliliz@gmail.com>
2016-05-04 20:12:02 -0700

    Work on Orders Controller show method. Deleted new method because it exists in the application controller now (Lisa and I worked this out)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,9 @@
 class OrdersController < ApplicationController
 
   def show
-    @order = OrderItem.find_by(current_user)
+    @order = OrderItem.find_by(user_id: current_user[:id])
     render :index
     #must show all order items in cart
     #must give editing option (delete & change quantity of items in cart) I think a form for each order item, AND for each order would suffice.
-  end
-
-  def new
-    if @order = Order.find_by(user_id: current_user) != nil
-
-    #RENDER INDEX??? Redirect around to whole cart. There is no form to fill out.
-    #Makes a new "cart"
-    #Has status of pending
-    #Has at least one item && item must have an "order quantity" of at least 1.
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ActiveRecord::Base
   has_many :order_items
   belongs_to :user
+
+  def add_to_cart(product)
+    @order_items = OrderItem.new(product_id: product.product_id, order_id: session[order_id:], quantity:  )
+  end
 end
