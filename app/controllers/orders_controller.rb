f5e1081f60a177d6d55eb39ffd5f6f72eadfb208
Jillian Boshart <Jilliliz@gmail.com>
2016-05-06 15:18:22 -0700

    Work on complete purchase button. Only works for logged in users atm

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,4 +6,9 @@ class OrdersController < ApplicationController
     #must show all order items in cart
     #must give editing option (delete & change quantity of items in cart) I think a form for each order item, AND for each order would suffice.
   end
+
+  def update
+    @order = Order.find_by(id: session[:order_id])
+    render :update
+  end
 end
