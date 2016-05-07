48f6faeab6c59c7118bfad5aa579e488a25537ab
justine <justine.winnie@gmail.com>
2016-05-07 15:37:59 -0700

    starting to add validations to reviews model

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,11 +2,26 @@ class Review < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
 
+  # t.integer  "product_id", null: false
+  # t.integer  "user_id"
+  # t.string   "review"
+  # t.integer  "rating",     null: false
+  # t.datetime "created_at", null: false
+  # t.datetime "updated_at", null: false
+
+  validates :user_id, presence: true, uniqueness: true
+  # validates :review??
+  validates :rating, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 6 }
+
   def get_user_name
     if user
       user.name
     else
       "Guest user"
     end
+  end
+
+  def display_name(user)
+    name = @user.name || "guest"
   end
 end
