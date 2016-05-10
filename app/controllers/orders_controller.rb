52d4ee8d464661581fd9fa9242eb4bac35355c69
melissajimison <melissajimison@gmail.com>
2016-05-10 15:52:42 -0700

    deleted the file index.html.erb  from orders view, and the index method from the Orders controler

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,4 @@
 class OrdersController < ApplicationController
-  def index
-    @user = User.find(session[:user_id])
-    @products_i_sell = Product.where(user_id: session[:user_id])
-    @my_order_items = OrderItem.where(product_id: @products_i_sell)
-    @total_revenue = @my_order_items.map { |item| item.product.price }.sum
-    @grouped_by_status = @my_order_items.group_by { |post| post.order.order_state}
-    @grouped_by_order = @my_order_items.group_by { |post| post.order.id}
-    @my_orders = @grouped_by_order.keys
-
-    scope = Order.where(id: @my_orders)
-
-    if params[:order_state]
-      scope = scope.where(order_state: params[:order_state])
-    end
-
-    @orders = scope
-  end
-
-
   def show
     @order = Order.find_by(id: session[:order_id])
     render :show
