0792ec35d5b033f2ec9583708cf4ce3f6f627ac3
justine <justine.winnie@gmail.com>
2016-05-10 16:51:52 -0700

    got guest/reg user displaying correctly using helper method.

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,11 +6,12 @@ class Review < ActiveRecord::Base
   # # validates :review??
   # validates :rating, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 6 }
 
+  # call this on an instance of review
   def get_user_name
-    if @user
-      @user.name
-    else
+    if self.user_id == nil || self.user_id == 0
       "Guest user"
+    else
+      User.find(self.user_id).name
     end
   end
 
