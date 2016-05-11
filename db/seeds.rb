f8d811a75df77605b7a8be55ab38a6616c8076e4
justine <justine.winnie@gmail.com>
2016-05-11 09:24:47 -0700

    added img urls to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,15 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 products = [
-  { name: "Flora Stone Maxi Dress", price: 35600, user_id: 1, description: "V-neck maxi in tropical print featuring adjustable spaghetti straps and center cutout with tie at bust", stock:20  },
-  { name: "Eyelet-Yoke Top", price: 4500, user_id: 1, description: "Sleeveless top in floral motif featuring crochet yoke and vertical insets at front and back", stock:20  },
-  { name: "Tree of Life Pendant Necklace", price: 9130, user_id: 2, description: "Sterling silver necklace featuring polished pendant with gold-plated tree at center and diamond-studded leaves", stock:20  },
-  { name: "Bold Stainless Steel Bracelet Watch", price: 55000, user_id: 2, description: "Elegant brushed stainless steel watch featuring white sunray dial with signature dot at 12 o'clock", stock:20  },
-  { name: "Cedar Street Bag", price: 28000, user_id: 3, description: "Colorful faux-leather satchel featuring hardware feet and removable cross-body strap", stock:20  },
-  { name: "Family Tree Personalized Names Necklace", price: 2900, user_id: 3, description: "Personalized family necklace with copper family tree charm in silver washer by DistinctlyIvy", stock:20  },
-  { name: "Ally Secret Trellis Long Silk Scarf", price: 15900 , user_id: 4, description: "Silk scarf in windowpane gingham with flowery ombre-tone border", stock:20  },
-  { name: "Good Vibes Crinkle Wrap", price: 15900 , user_id: 4, description: "Lightweight color-block scarf featuring delicate eyelash fringe and glittering ends", stock:20  },
-  { name: "Carrson Dress Sandal", price: 90000 , user_id: 5, description: "Ankle-strap sandal in rich leather featuring covered block heel and single toe", stock:20  },
-
+  { name: "Flora Stone Maxi Dress", price: 35600, user_id: 1, description: "V-neck maxi in tropical print featuring adjustable spaghetti straps and center cutout with tie at bust", stock:20, photo_url: "http://imgc.allpostersimages.com/images/P-473-488-90/67/6789/DX4I100Z/posters/kike-calvo-a-dog-in-traditional-colombian-country-dress-at-the-silleteros-parade.jpg"  },
+  { name: "Eyelet-Yoke Top", price: 4500, user_id: 1, description: "Sleeveless top in floral motif featuring crochet yoke and vertical insets at front and back", stock:20, photo_url: "http://assets.charmingshoppes.com/is/image/LaneBryant/pr_175047_1A?$product_main$&wid=354&hei=460"  },
+  { name: "Tree of Life Pendant Necklace", price: 9130, user_id: 2, description: "Sterling silver necklace featuring polished pendant with gold-plated tree at center and diamond-studded leaves", stock:20, photo_url: "https://s-media-cache-ak0.pinimg.com/736x/f2/84/3f/f2843ff0aecbd268e54c56c36c52e67e.jpg"  },
+  { name: "Bold Stainless Steel Bracelet Watch", price: 55000, user_id: 2, description: "Elegant brushed stainless steel watch featuring white sunray dial with signature dot at 12 o'clock", stock:20, photo_url: "http://i464.photobucket.com/albums/rr5/senraw/fluffy_zps7817e376.jpg"  },
+  { name: "Cedar Street Bag", price: 28000, user_id: 3, description: "Colorful faux-leather satchel featuring hardware feet and removable cross-body strap", stock:20, photo_url: "http://www.bagsstyleblog.com/wp-content/uploads/2014/05/09/6/1645-BlingDay-Genuine-Leather-Multi-colored-Rhinestone-Flower-Patchwork-Large-Satchel-Bag-for-Women-1.jpg"  },
+  { name: "Family Tree Personalized Names Necklace", price: 2900, user_id: 3, description: "Personalized family necklace with copper family tree charm in silver washer by DistinctlyIvy", stock:20, photo_url: "https://img0.etsystatic.com/000/0/5399945/il_570xN.182753110.jpg"  },
+  { name: "Ally Secret Trellis Long Silk Scarf", price: 15900 , user_id: 4, description: "Silk scarf in windowpane gingham with flowery ombre-tone border", stock:20, photo_url: "http://rlv.zcache.com/vintage_christmas_gingham_puppy_dog_stuffed_animal_scarf-rf3357b105f344a08ab2f7609def6c491_jpj4v_8byvr_630.jpg?view_padding=%5B285%2C0%2C285%2C0%5D"  },
+  { name: "Good Vibes Crinkle Wrap", price: 15900 , user_id: 4, description: "Lightweight color-block scarf featuring delicate eyelash fringe and glittering ends", stock:20, photo_url: "https://img0.etsystatic.com/060/0/6921854/il_214x170.719502622_lxkw.jpg" },
+  { name: "Carrson Dress Sandal", price: 90000 , user_id: 5, description: "Ankle-strap sandal in rich leather featuring covered block heel and single toe", stock:20, photo_url: "https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2012/1/18/1326907445081/Paula-Deen-riding-a-stick-007.jpg?w=620&q=55&auto=format&usm=12&fit=max&s=acddaa78287bd521cdfb7aa108d86c05"  },
 ]
 
 products.each do |product|
@@ -36,7 +35,7 @@ categories.each do |category|
   Category.create category
 end
 
-p_one = Product.find(1) 
+p_one = Product.find(1)
 p_one.categories << Category.find_by_name("Dress")
 p_two = Product.find(2)
 p_two.categories << Category.find_by_name("Blouse")
