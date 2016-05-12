2b118ff02b9c5640ced59b6651bbe8ebd5854da5
justine <justine.winnie@gmail.com>
2016-05-12 14:54:55 -0700

    creating get_user_name method test

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,10 +5,9 @@ class Review < ActiveRecord::Base
   validates :review, length: {maximum: 500, message: "Review has exceeded length."}, allow_blank: true, format: { with: /\A[a-zA-Z1-9]+\z/, message: "Only allows letters" }
 
   validates :rating, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 6 }
-  
+
   validates_associated :product
 
-  # call this on an instance of review
   def get_user_name
     if self.user_id == nil || self.user_id == 0
       "Guest user"
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -2,22 +2,12 @@ require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
 
-  # test "validations: rating must not be blank" do
-  #   review = reviews(:review_4)
-  #   assert_not_nil review.rating
-  # end
-
   test "validations: review can be left blank" do
     review = Review.new(rating: 3)
     assert review.valid?
   end
 
-  # test "validations: review can be left blank" do
-  #   review = reviews(:review_5)
-  #   assert review.valid?
-  # end
-
-  test "validations: user id must be an integer, not a string" do
+  test "validations: user id must be an integer" do
     review = reviews(:review_2)
     assert Integer, review.rating
   end
@@ -27,7 +17,10 @@ class ReviewTest < ActiveSupport::TestCase
     assert_not review.valid?
   end
 
-
+  test "get_user_name" do
+    review = reviews(:review_4)
+    review.get_user_name
+  end
 
 # assert(expression, fail_message)
 # assert_not(expression, fail_message)
