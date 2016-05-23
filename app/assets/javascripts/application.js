47e12cf455f7aa3c7f63b4967b3886cda56ba273
Anna Wilson <annie.marie.mason@gmail.com>
2016-05-23 15:09:20 -0700

    js bug fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree
-= require bootstrap-dropdown
-= require bootstrap
+// = require bootstrap-dropdown
+// = require bootstrap
 
 // jQuery(function($) {
 // $("tr[data-link]").click(function() {
