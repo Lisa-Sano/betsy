d61d6d4c8632f6abb3625c1d9c30d090b8f26798
Deirdre Storck <deirdre.storck@gmail.com>
2016-05-24 11:07:12 -0700

    made migration to add height weight length width to product and updated seeds

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512212224) do
+ActiveRecord::Schema.define(version: 20160524172155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,10 @@ ActiveRecord::Schema.define(version: 20160512212224) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.boolean  "retired",     default: false
+    t.float    "weight"
+    t.float    "height"
+    t.float    "length"
+    t.float    "width"
   end
 
   create_table "reviews", force: :cascade do |t|
