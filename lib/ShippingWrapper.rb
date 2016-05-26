5f71df511da130f3067d7c453d7b6310907c3b97
Nadine Curry <nlcurry@gmail.com>
2016-05-26 10:41:01 -0700

    don't need choose method

--- lib/ShippingWrapper.rb
+++ lib/ShippingWrapper.rb
@@ -4,26 +4,13 @@ class ShippingWrapper
  BASE_URL = ""
 
   def self.get_rates(destination, weight)
-    origin = {:state => "WA", :city => "Seattle", :zip => "98103"}
-    #standardizing shipping dimensions
-    package = {:weight => weight, :length => 20, :width => 20, :height => 20, :units => "metric"}
-
-    data = HTTParty.get(BASE_URL + "",
-         :headers => { 'Accept' => 'application/json' },
-         :body => { :destination => {:state => , :city => , :zip =>  },
-                   :origin => origin,
-                   :package => package})
-    return data
-  end
-
-  def self.choose(destination, weight)
-    origin = {:state => "WA", :city => "Seattle", :zip => "98103"}
+    origin = {:country => "US", :state => "WA", :city => "Seattle", :zip => "98103"}
     #standardizing shipping dimensions
     package = {:weight => weight, :length => 20, :width => 20, :height => 20, :units => "metric"}
 
     data = HTTParty.post(BASE_URL + "",
          :headers => { 'Accept' => 'application/json' },
-         :body => { :destination => {:state => , :city => , :zip =>  },
+         :body => { :destination => {:country => 'US', :state => destination.state, :city => destination.city, :zip => destination.zip  },
                    :origin => origin,
                    :package => package})
     return data
