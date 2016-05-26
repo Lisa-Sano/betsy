ff024198662605635f88e47534b415cf0f1bc7dd
Nadine Curry <nlcurry@gmail.com>
2016-05-25 20:51:44 -0700

    added some conditionals to update method so it can be used twice

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,10 +13,14 @@ class OrdersController < ApplicationController
   def update
     @order = Order.find_by(id: session[:order_id])
     @order.assign_attributes(order_update_params[:order])
-    if params[:order][:last_four_cc] != nil
-      @order.last_four_cc = params[:order][:last_four_cc][-4..-1]
-    end
-    if @order.save
+
+    #saves last four of CC if passing through update the first time
+    @order.last_four_cc = params[:order][:last_four_cc][-4..-1] if params[:order][:last_four_cc] != nil
+
+    #updates status if shipping info exists(after order confirmation)
+    @order.assign_attributes(order_state: "paid") if params[:order][:carrier_code] != nil
+
+    if @order.save && params[:order][:carrier_code] != nil
       reset_cart
       render :order_confirmation
     else
@@ -25,21 +29,9 @@ class OrdersController < ApplicationController
     end
   end
 
-  def confirm_pay
-    @order = Order.find_by(id: session[:order_id])
-    @order.assign_attributes(order_update_params[:order])
-    @order.assign_attributes(order_state: "paid")
-    @order.last_four_cc = params[:order][:last_four_cc][-4..-1]
-    if @order.save
-      reset_cart
-      render :order_confirmation
-    else
-      @user = User.find_by(id: session[:user_id])
-      render :edit
-    end
   private
 
   def order_update_params
-    params.permit(order: [:name, :email, :address, :city, :state, :zip, :card_name, :cc_cvv, :billing_zip, :cc_exp_month, :cc_exp_year])
+    params.permit(order: [:name, :email, :address, :city, :state, :zip, :card_name, :cc_cvv, :billing_zip, :cc_exp_month, :cc_exp_year, :tracking_info, :carrier_code, :shipping_cost])
   end
 end
