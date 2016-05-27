5ae17098221da225cee534cb9865fd84a0c9f6ff
Deirdre Storck <deirdre.storck@gmail.com>
2016-05-27 11:15:36 -0700

    refactored what we send to shipping api to take out unnecessary info

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,21 +25,20 @@ class OrdersController < ApplicationController
 
   def displayrates
     @order = Order.find_by(id: session[:order_id])
-    params[:order][:state] = @order.state
-    params[:order][:city] = @order.city
-    params[:order][:zip] = @order.zip
+    info = {:order => {}}
+    info[:order][:state] = @order.state
+    info[:order][:city] = @order.city
+    info[:order][:zip] = @order.zip
 
-    params[:order][:origin]= { state: "WA", zip: 98112, city: "Seattle" }
-    params[:order][:orderitems] = @order.hashify
-    @params = params.to_json
-
+    info[:order][:origin]= { state: "WA", zip: 98112, city: "Seattle" }
+    info[:order][:orderitems] = @order.hashify
+    @params = info.to_json
+  
 
     @results = HTTParty.post("http://localhost:3000/shipping/rates",
     :body => @params,
      :headers => { 'Content-Type' => 'application/json' }
     )
-
-
     if @results.code > 200
       @error = @results.parsed_response["error"]
       return false
