6d273cbaf79661bf38ae47c22da74fbeacb444af
Nadine Curry <nlcurry@gmail.com>
2016-05-26 20:17:40 -0700

    gave name to paths

--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,8 @@ Rails.application.routes.draw do
   end
 
   resources :orders, :only => [:show, :edit, :update]
-  get "/order/:id/shipping" => "orders#shipping"
-  patch "/orders/:id/shipping" => "orders#confirmation"
+  get "/order/:id/shipping" => "orders#shipping", as :shipping
+  patch "/orders/:id/shipping" => "orders#confirmation", as :confirmation
 
   resources :order_items, :only => [:show]
 
