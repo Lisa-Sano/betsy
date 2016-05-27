4624dde49276e77196ae3884a5bcbf33be7c8947
Nadine Curry <nlcurry@gmail.com>
2016-05-27 10:12:35 -0700

    no base_url

--- lib/ShippingWrapper.rb
+++ lib/ShippingWrapper.rb
@@ -1,14 +1,13 @@
 require 'HTTParty'
 
 class ShippingWrapper
- BASE_URL = ""
 
   def self.get_rates(destination, weight)
     origin = {:country => "US", :state => "WA", :city => "Seattle", :zip => "98103"}
     #standardizing shipping dimensions
     package = {:weight => weight, :length => 20, :width => 20, :height => 20, :units => "metric"}
 
-    data = HTTParty.get(BASE_URL + "",
+    data = HTTParty.get("https://betsy-shipping-service.herokuapp.com/shipping_rates",
          :headers => { 'Accept' => 'application/json' },
          :body => { :destination => {:country => 'US', :state => destination.state, :city => destination.city, :zip => destination.zip },
                    :origin => origin,
